5a0bd922cd2910eb02be9f549c9ede651d7588b9
nabesoma <somanabe0719@gmail.com>
2024-11-01 18:18:40 +0900

    PHP kadai_007 upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>PHP 課題７</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+    <?php
+    $vegetable=['name'=>'onion', 'price'=>200, 'weight'=>160];
+
+    print_r($vegetable);
+    ?>
+
+  </body>
+</html>
