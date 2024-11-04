040485a1fde918d40fc33aa9fc55d16a72f54ca6
nabesoma <somanabe0719@gmail.com>
2024-11-04 22:09:51 +0900

    PHP kadai_sort upload3

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -7,22 +7,34 @@
   <body>
 
     <?php
-    // ソートする配列を宣言
-    $nums = [15, 4, 18, 23, 10 ];
 
-    // 昇順にソート
-    echo '昇順にソートします<br>';
-    sort($nums);
-    foreach($nums as $num){
-      echo $num.'<br>';
-    };
+      // ソート関数を定義する
+      function sort_2way($array, $order){
+        
+        if($order===TRUE){
+          // 昇順にソート
+          echo '昇順にソートします<br>';
+          sort($array);
+          foreach($array as $num){
+            echo $num.'<br>';
+          }
+        }else{
+          // 降順にソート
+          echo '降順にソートします<br>';
+          rsort($array);
+          foreach($array as $num){
+            echo $num.'<br>';
+          }
+        }
 
-    // 降順にソート
-    echo '降順にソートします<br>';
-    rsort($nums);
-    foreach($nums as $num){
-      echo $num.'<br>';
-    };
+      }
+
+      // ソートする配列を宣言
+      $nums = [15, 4, 18, 23, 10 ];
+
+      // ソート関数を呼び出す
+      echo sort_2way($nums,TRUE);
+      echo sort_2way($nums,FALSE);
 
     ?>
 
