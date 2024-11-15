6fcfa32d200651652c847a31fa6e3a0477aa791a
nabesoma <somanabe0719@gmail.com>
2024-11-15 17:55:13 +0900

    php_web_app_kadai_upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>PHP Webアプリ制作課題</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+    
+  </body>
+</html>
